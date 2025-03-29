6c33f2bbba4e27a3083b5ad61f146b424e801f9b
MEENAKSHI2113007 <151904758+MEENAKSHI2113007@users.noreply.github.com>
2025-03-29 09:53:51 +0530

    Create App.js

---
+++ sign-language-trainer/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ImageDisplay from "./components/ImageDisplay";
+import CameraFeed from "./components/CameraFeed";
+
+const App = () => {
+  const [currentWord, setCurrentWord] = useState("");
+  const [recognizedSigns, setRecognizedSigns] = useState([]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Name the Picture</h1>
+      <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+        <ImageDisplay 
+          setCurrentWord={setCurrentWord} 
+          recognizedSigns={recognizedSigns} 
+          setRecognizedSigns={setRecognizedSigns} 
+        />
+        <CameraFeed onCapture={(sign) => setRecognizedSigns([...recognizedSigns, sign])} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
